48f641c101c391a7e0d8f7ad58bcbe2e50a5ebb9
Breno Mendes <b.mendesbernardo2020@gmail.com>
2022-11-25 18:18:20 -0300

    ladinga-page-apple

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+function circleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+console.log('Hello..')
